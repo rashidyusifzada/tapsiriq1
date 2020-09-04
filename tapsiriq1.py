2b0ba71938eb5f8b405b7d4642b2ed66dde6c4b2
Rashid <rashidyusifzada@gmail.com>
2020-09-04 21:27:42 +0400

    tapsiriq1

---
+++ tapsiriq1.py
@@ -0,0 +1,35 @@
+# İstifadəcinin daxil etdiyi edede geder olan ededlerin cemini ekrana cap edən kod yazın
+# İstifadəçini daxil edə biləcəyi bütün səhv  ehtimallı nəzərə alınıb ona uygun xeta mesaji yazdirilmalıdır ekrana
+
+# cem = 0
+# eded = input("neceye kimi ededleri cemlemek isteyirsiniz? :")
+# eded = int(eded)
+#
+# i = 0
+# while i <= eded:
+#     cem = cem + i
+#     i = i + 1
+#
+# print(cem)
+
+cavab = input("nece eded daxil etmek isteyirsiniz?: ")
+cavab = int(cavab)
+tekrar = 1
+print(tekrar)
+while tekrar <= cavab:
+    eded = input("eded daxil edin: ")
+    tekrar = tekrar + 1
+
+print(tekrar)
+
+# eded = input("eded daxil edin: ")
+# eded2 = input("eded daxil edin: ")
+# eded3 = input("eded daxil edin: ")
+#
+# ededler = []
+#
+# ededler.append(eded)
+# ededler.append(eded2)
+# ededler.append(eded3)
+#
+# print(ededler)
